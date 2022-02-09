b4c2c31f9ce04b70b827d90805712186adde6f1c
omar <omar.akl14@gmail.com>
2022-02-09 16:04:01 +0200

    pushing-sidebar

--- blog/settings.py
+++ blog/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,7 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'blog',
-        'USER':'samahg',
+        'USER':'omar',
         'PASSWORD':'passw0rd',
     }
 }
