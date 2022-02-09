3878a5a469532b846abe3ecb481159307e9d4076
youmna <yoma.samy98@gmail.com>
2022-02-09 23:00:49 +0200

    register login

--- djapp/urls.py
+++ djapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 
     path('home/', views.home, name='home' ) ,
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login' )
 ]
--- djapp/views.py
+++ djapp/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from multiprocessing import context
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
 def home(request):
     return render(request,'djapp/home.html')
+
+def register(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                return redirect('login')
+
+    context = { 'form': form}
+    return render(request, 'djapp/register.html', context)
+
+def login(request):
+    context= {}
+    return render(request, 'djapp/login.html', context)
