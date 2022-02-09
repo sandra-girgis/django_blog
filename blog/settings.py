e88d854ded964df5740df191fe4442a4e01ba681
samah gaber <samahgabermo@gmail.com>
2022-02-09 13:26:12 +0200

    update

--- blog/settings.py
+++ blog/settings.py
@@ -51,11 +51,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'blog.urls'
-
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ djapp/urls.py
@@ -0,0 +1,8 @@
+from unicodedata import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('home/', views.home, name='home' ) ,
+]
