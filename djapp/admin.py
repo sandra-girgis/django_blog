213f1217082f5a1c3bbe143d9b0c79800db8045f
rehabmohamed19 <rehabmohamedaziz99@gmail.com>
2022-02-10 05:36:13 -0800

    model:comment & admin:word

--- djapp/admin.py
+++ djapp/admin.py
@@ -18,7 +18,17 @@ class CommentAdmin(admin.ModelAdmin):
     )
     list_display = ('Text','Post_id','User_id') #'Time',
 
+#rehab
+class WordAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ['Category Details',{'fields':['Name']}],
+    )
+    list_display = ('Name',)
+    search_fields = ['Name']
+
 #sandra
 admin.site.register(Comment,CommentAdmin)
 #youmna
 admin.site.register(Tag,TagAdmin)
+#rehab
+admin.site.register(Word,WordAdmin)
--- djapp/models.py
+++ djapp/models.py
@@ -19,6 +19,17 @@ class Post(models.Model):
     Tags = models.ManyToManyField(Tag)
     def __str__(self):
         return self.Title
+
+
+#rehab
+class Comment(models.Model):
+    Text = models.CharField(max_length = 100, null = False)
+    Time = models.DateTimeField()
+    Post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    def _str_(self):
+        return self.Text
+
 #sandra
 class Postlike(models.Model):
     Islike = models.BooleanField(default=False)
