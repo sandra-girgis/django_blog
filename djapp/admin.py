5231338b8f7f360becd2d1359d99061709a54e07
ToTa <tota@totajo1999@gmail.com>
2022-02-10 15:09:11 +0200

    models:postlike & admin:comment

--- djapp/admin.py
+++ djapp/admin.py
@@ -1,3 +1,13 @@
+#sandra
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#sandra
+class CommentAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ['User Details',{'fields':['Text','Post_id','User_id']}], #'Time',
+    )
+    list_display = ('Text','Post_id','User_id') #'Time',
+
+#sandra
+admin.site.register(Comment,CommentAdmin)
--- djapp/models.py
+++ djapp/models.py
@@ -1,3 +1,15 @@
-from django.db import models
-
+#sandra
 # Create your models here.
+from django.db import models
+# Create your models here.
+# models classes are tables
+# first to create
+
+#sandra
+class Postlike(models.Model):
+    Islike = models.BooleanField(default=False)
+    Isdislike = models.BooleanField(default=False)
+    Post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    User_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    Islike.boolean = True
+    Isdislike.boolean = True
