4ebfadbf4edc872b3a99de899ac9c9823213a447
Reem <reem@shkeepreem@gmail.com>
2022-02-10 15:43:24 +0200

    admin,Class:User and category

--- djapp/admin.py
+++ djapp/admin.py
@@ -2,6 +2,22 @@
 from django.contrib import admin
 from .models import *
 
+# Register your models here.
+
+#REEM
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ['User Details',{'fields':['Username','Password','Email','Isadmin','Isblocked']}], #show on create
+    )
+    list_display = ('Username','Password','Email','Isadmin','Isblocked') # show users list
+    list_filter = ['Username','Email','Isadmin','Isblocked'] # advanced
+    search_fields = ['Username','Email','Isadmin','Isblocked'] # advanced
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = (
+        ['Category Details',{'fields':['Name']}],
+    )
+    list_display = ('Name',)
+    search_fields = ['Name']
 
 #youmna
 class TagAdmin(admin.ModelAdmin):
@@ -26,6 +42,10 @@ class WordAdmin(admin.ModelAdmin):
     list_display = ('Name',)
     search_fields = ['Name']
 
+
+#REEM
+admin.site.register(User,UserAdmin) # table.model , customized model
+admin.site.register(Category,CategoryAdmin)
 #sandra
 admin.site.register(Comment,CommentAdmin)
 #youmna
