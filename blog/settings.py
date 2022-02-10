33c8b441344eb6847716b735530aeceffe6e99a3
samah gaber <samahgabermo@gmail.com>
2022-02-10 14:35:51 +0200

    update

--- blog/settings.py
+++ blog/settings.py
@@ -77,8 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'blog',
-        'USER':'rehab',
-        'PASSWORD':'rehab',
+        'USER':'samahg',
+        'PASSWORD':'passw0rd',
     }
 }
 
